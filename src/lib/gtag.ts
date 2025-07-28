05dd9dd3358840c46e8a1e8a3058bff19a293a47
ruppstudent <pengseangsim210@gmail.com>
2025-07-28 09:08:08 +0700

    New

--- src/lib/gtag.ts
+++ src/lib/gtag.ts
@@ -1,35 +1,35 @@
 declare global {
   interface Window {
-	// eslint-disable-next-line @typescript-eslint/no-explicit-any
-	gtag: (...args: any[]) => void;
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    gtag: (...args: any[]) => void;
   }
 }
 
-export const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_ID || ''
+export const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_ID || "";
 // Log page views
 export const pageview = (url: string) => {
- if (!GA_TRACKING_ID) return
- window.gtag('config', GA_TRACKING_ID, {
- page_path: url,
- })
-}
+  if (!GA_TRACKING_ID) return;
+  window.gtag("config", GA_TRACKING_ID, {
+    page_path: url,
+  });
+};
 
 // Log specific events
 export const event = ({
- action,
- category,
- label,
- value,
+  action,
+  category,
+  label,
+  value,
 }: {
- action: string
- category: string
- label: string
- value: number
+  action: string;
+  category: string;
+  label: string;
+  value: number;
 }) => {
- if (!GA_TRACKING_ID) return
- window.gtag('event', action, {
- event_category: category,
- event_label: label,
- value: value,
- })
-}
+  if (!GA_TRACKING_ID) return;
+  window.gtag("event", action, {
+    event_category: category,
+    event_label: label,
+    value: value,
+  });
+};
